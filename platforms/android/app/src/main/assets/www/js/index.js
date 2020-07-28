7839469227d7fc5b918ef3aaeb887b956eb37122
firstArachne <firstArachne0116@gmail.com>
2020-07-29 02:04:49 +1000

    contacts received

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -27,20 +9,28 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+        var options      = new ContactFindOptions();
+        options.filter   = "";
+        options.multiple = true;
+        var fields = ["displayName","phoneNumbers"];
+        navigator.contacts.find(fields, onSuccessContact, onErrorContact, options);
     },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    onSuccessContact: function(contacts) {
+        // for (var i = 0; i < contacts.length; i ++){
+        //     var node = document.createElement("LI");                 // Create a <li> node
+        //     var textnode = document.createTextNode(contacts[i].displayName + " " + contacts[i].phoneNumber);         // Create a text node
+        //     node.appendChild(textnode);                              // Append the text to <li>
+        //     document.getElementById("contacts").appendChild(node); 
+        // }
+        // var node = document.createTextNode(JSON.stringify(contacts));
+        // document.getElementById("main").appendChild(node);
+        alert('Found ' + contacts.length + ' contacts.');
+    },
+    onErrorContact: function() {
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    },
 
-        console.log('Received Event: ' + id);
-    }
 };
 
 app.initialize();
--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -9,6 +27,8 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+        
         var options      = new ContactFindOptions();
         options.filter   = "";
         options.multiple = true;
@@ -17,20 +37,25 @@ var app = {
     },
 
     onSuccessContact: function(contacts) {
-        // for (var i = 0; i < contacts.length; i ++){
-        //     var node = document.createElement("LI");                 // Create a <li> node
-        //     var textnode = document.createTextNode(contacts[i].displayName + " " + contacts[i].phoneNumber);         // Create a text node
-        //     node.appendChild(textnode);                              // Append the text to <li>
-        //     document.getElementById("contacts").appendChild(node); 
-        // }
-        // var node = document.createTextNode(JSON.stringify(contacts));
-        // document.getElementById("main").appendChild(node);
-        alert('Found ' + contacts.length + ' contacts.');
+        var contactsElement = parentElement.querySelector('.contacts');
+        contactsElement.setAttribute('style', 'display:block;');
     },
-    onErrorContact: function() {
+
+    onErrorContact: function(err) {
 
     },
 
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
 };
 
 app.initialize();
